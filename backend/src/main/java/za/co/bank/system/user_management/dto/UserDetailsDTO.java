c5a8b0dd88376aa0c14d00f3a2803086449f94e0
Given Mutshidza <gimutshjhb024@student.wethinkcode.co.za>
2025-02-03 13:37:20 +0200

    Created: dto to hold user's details

--- backend/src/main/java/za/co/bank/system/user_management/dto/UserDetailsDTO.java
+++ backend/src/main/java/za/co/bank/system/user_management/dto/UserDetailsDTO.java
@@ -1,4 +1,61 @@
 package za.co.bank.system.user_management.dto;
 
+import java.time.LocalDate;
+
 public class UserDetailsDTO {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String idNumber;
+    private String address;
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
