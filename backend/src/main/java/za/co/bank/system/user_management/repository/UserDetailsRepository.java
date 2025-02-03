5f3b3adb84b571240885c1796c2df94e75dbd044
Given Mutshidza <gimutshjhb024@student.wethinkcode.co.za>
2025-02-03 13:32:08 +0200

    Added: repo to handle all user details saving

--- backend/src/main/java/za/co/bank/system/user_management/repository/UserDetailsRepository.java
+++ backend/src/main/java/za/co/bank/system/user_management/repository/UserDetailsRepository.java
@@ -1,4 +1,9 @@
 package za.co.bank.system.user_management.repository;
 
-public interface UserDetailsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.co.bank.system.user_management.entity.UserDetails;
+
+@Repository
+public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
 }
