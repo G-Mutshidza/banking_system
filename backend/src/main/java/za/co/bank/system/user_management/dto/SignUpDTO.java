475c1ffc3da23dcad8a9258f4114ef86efa9e669
Given Mutshidza <gimutshjhb024@student.wethinkcode.co.za>
2025-02-03 13:30:34 +0200

    Added: created a dto to hold sign up data

--- backend/src/main/java/za/co/bank/system/user_management/dto/SignUpDTO.java
+++ backend/src/main/java/za/co/bank/system/user_management/dto/SignUpDTO.java
@@ -1,4 +1,32 @@
 package za.co.bank.system.user_management.dto;
 
-public class UserDTO {
+public class SignUpDTO {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
