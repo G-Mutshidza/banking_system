69811c6685d670ebb0a22e96dda4e68cc9dbf5a9
Lindokuhle Hlatshwayo <lindokuhlehlatshwayo@gmail.com>
2025-02-10 08:26:00 +0200

    Add entity and repository classes for client onboarding features
     Date:      Mon Feb 10 08:26:00 2025 +0200
     interactive rebase in progress; onto 6eb585e
     Last command done (1 command done):
        edit 74291b8 Add entity and repository classes for client onboarding features
     No commands remaining.
     You are currently editing a commit while rebasing branch 'main' on '6eb585e'.
     Changes to be committed:
    	modified:   backend/src/main/java/za/co/bank/system/client_onboarding/entity/Client.java
    	modified:   backend/src/main/java/za/co/bank/system/client_onboarding/entity/Consent.java
    	modified:   backend/src/main/java/za/co/bank/system/client_onboarding/repository/ConsentRepository.java
    	new file:   backend/src/main/java/za/co/bank/system/client_onboarding/repository/IdentificationRepository.java
    
    	new file:   backend/src/main/java/za/co/bank/system/client_onboarding/repository/RiskComplianceRepository.java

--- backend/src/main/java/za/co/bank/system/client_onboarding/repository/SecurityRepository.java
+++ backend/src/main/java/za/co/bank/system/client_onboarding/repository/SecurityRepository.java
@@ -1,9 +1,9 @@
 package za.co.bank.system.client_onboarding.repository;
 
-import za.co.bank.system.client_onboarding.entity.Consent;
+import za.co.bank.system.client_onboarding.entity.Security;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConsentRepository extends JpaRepository<Consent, Long> {
+public interface SecurityRepository extends JpaRepository<Security, Long>{   
 }
--- backend/src/main/java/za/co/bank/system/client_onboarding/repository/VerificationRepository.java
+++ backend/src/main/java/za/co/bank/system/client_onboarding/repository/VerificationRepository.java
@@ -1,9 +1,9 @@
 package za.co.bank.system.client_onboarding.repository;
 
-import za.co.bank.system.client_onboarding.entity.Consent;
+import za.co.bank.system.client_onboarding.entity.Verification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConsentRepository extends JpaRepository<Consent, Long> {
+public interface VerificationRepository extends JpaRepository<Verification, Long>{   
 }
