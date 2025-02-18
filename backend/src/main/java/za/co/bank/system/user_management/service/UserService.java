4ee3fa9dc4bfe2c445c3943638410636f4fa3c6e
Given Mutshidza <gimutshjhb024@student.wethinkcode.co.za>
2025-02-18 12:14:06 +0200

    Modified: added role to my registration

--- backend/src/main/java/za/co/bank/system/user_management/service/UserService.java
+++ backend/src/main/java/za/co/bank/system/user_management/service/UserService.java
@@ -1,32 +1,38 @@
 package za.co.bank.system.user_management.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import za.co.bank.system.user_management.dto.SignUpDTO;
 import za.co.bank.system.user_management.dto.UserDetailsDTO;
+import za.co.bank.system.user_management.entity.Role;
 import za.co.bank.system.user_management.entity.User;
 import za.co.bank.system.user_management.entity.UserProfile;
-import za.co.bank.system.user_management.repository.UserDetailsRepository;
+import za.co.bank.system.user_management.repository.RoleRepository;
+import za.co.bank.system.user_management.repository.UserProfileRepository;
 import za.co.bank.system.user_management.repository.UserRepository;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Service
 public class UserService{
 
     private final UserRepository userRepository;
-    private final UserDetailsRepository userDetailsRepository;
+    private final UserProfileRepository userProfileRepository;
+    private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(@Qualifier("mongoUserRepository") UserRepository userRepository, @Qualifier("mongoUserRepository") UserDetailsRepository userDetailsRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, UserProfileRepository userProfileRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
-        this.userDetailsRepository = userDetailsRepository;
+        this.userProfileRepository = userProfileRepository;
+        this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User registerUser(SignUpDTO signUpDTO) {
+    public User registerUser(SignUpDTO signUpDTO, Set<String> roleNames) {
         if (userRepository.existsByUsername(signUpDTO.getUsername())) {
             throw new IllegalArgumentException("Username already taken.");
         }
@@ -38,6 +44,14 @@ public class UserService{
         user.setUsername(signUpDTO.getUsername());
         user.setEmail(signUpDTO.getEmail());
         user.setPassword(passwordEncoder.encode(signUpDTO.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        for(String roleName : roleNames) {
+            Role role = roleRepository.findByName(roleName)
+                    .orElseThrow(() -> new IllegalArgumentException("Role " + roleName + " not found!"));
+            roles.add(role);
+        }
+        user.setRoles(roles);
 
         return userRepository.save(user);
     }
@@ -51,12 +65,12 @@ public class UserService{
         userProfile.setIdNumber(userDetailsDTO.getIdNumber());
         userProfile.setAddress(userDetailsDTO.getAddress());
         userProfile.setPhoneNumber(userDetailsDTO.getPhoneNumber());
-        userProfile.setUserId(user.getId());
+        userProfile.setUser(user);
 
-        return userDetailsRepository.save(userProfile);
+        return userProfileRepository.save(userProfile);
     }
 
-    public User getUserById(String id) {
+    public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
     }
 
