41ff38cf8651b8e9d486ad8179457d9f7c1dcf15
Given Mutshidza <gimutshjhb024@student.wethinkcode.co.za>
2025-02-18 12:10:54 +0200

    Modified: reverted from MongoDB to JPA

--- backend/src/main/java/za/co/bank/system/user_management/repository/UserProfileRepository.java
+++ backend/src/main/java/za/co/bank/system/user_management/repository/UserProfileRepository.java
@@ -1,7 +1,6 @@
-package za.co.bank.system.user_management.repository.mongodb;
+package za.co.bank.system.user_management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import za.co.bank.system.user_management.entity.UserProfile;
 
