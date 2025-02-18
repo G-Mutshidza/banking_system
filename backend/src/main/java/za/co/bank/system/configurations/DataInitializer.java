321f32d6ce4757c789bcac764d9db86a26d58e38
Given Mutshidza <gimutshjhb024@student.wethinkcode.co.za>
2025-02-18 11:57:40 +0200

    Added: Component to initialize roles in the database

--- backend/src/main/java/za/co/bank/system/configurations/DataInitializer.java
+++ backend/src/main/java/za/co/bank/system/configurations/DataInitializer.java
@@ -1,4 +1,25 @@
 package za.co.bank.system.configurations;
 
-public class DataInitializer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import za.co.bank.system.user_management.entity.Role;
+import za.co.bank.system.user_management.repository.RoleRepository;
+
+public class DataInitializer implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public DataInitializer(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+        if (roleRepository.count() == 0) {
+            roleRepository.save(new Role("ADMIN"));
+            roleRepository.save(new Role("BANKER"));
+            roleRepository.save(new Role("CLIENT"));
+        }
+    }
 }
