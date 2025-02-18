b152c7b277289c9b125cf54a2f81c7a2f8a19139
Given Mutshidza <gimutshjhb024@student.wethinkcode.co.za>
2025-02-18 12:03:38 +0200

    Added: Entity for roles

--- backend/src/main/java/za/co/bank/system/user_management/entity/Role.java
+++ backend/src/main/java/za/co/bank/system/user_management/entity/Role.java
@@ -1,4 +1,25 @@
 package za.co.bank.system.user_management.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role(String name) {
+        this.name = name;
+    }
 }
