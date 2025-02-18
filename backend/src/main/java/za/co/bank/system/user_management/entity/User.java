5e1859bb4a73853f3b8f4f077d701d2e54b2edcd
Given Mutshidza <gimutshjhb024@student.wethinkcode.co.za>
2025-02-18 12:04:49 +0200

    Modified: added a roles table join

--- backend/src/main/java/za/co/bank/system/user_management/entity/User.java
+++ backend/src/main/java/za/co/bank/system/user_management/entity/User.java
@@ -3,34 +3,40 @@ package za.co.bank.system.user_management.entity;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Set;
 
 @Entity
-@Document(collection = "users")
+@Table(name = "users")
 @Data
 public class User implements UserDetails {
 
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Field("username")
+    @Column(nullable = false)
     private String username;
 
-    @Field("password")
+    @Column(nullable = false)
     private String password;
 
-    @Field("email")
+    @Column(nullable = false)
     private String email;
 
-    @OneToOne(mappedBy = "user")
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonManagedReference
     private UserProfile userProfile;
 
