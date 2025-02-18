309a1560bed3f1c3009912606bd821bff33fa453
Given Mutshidza <gimutshjhb024@student.wethinkcode.co.za>
2025-02-18 12:02:55 +0200

    Modified: added the roles var

--- backend/src/main/java/za/co/bank/system/user_management/dto/SignUpDTO.java
+++ backend/src/main/java/za/co/bank/system/user_management/dto/SignUpDTO.java
@@ -2,10 +2,13 @@ package za.co.bank.system.user_management.dto;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class SignUpDTO {
 
     private String username;
     private String email;
     private String password;
+    private Set<String> roleNames;
 }
